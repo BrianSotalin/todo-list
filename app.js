05cf08e16c567a422e1092547b0c1d9320738504
Brian Sotalin <dannysotalin@gmail.com>
2022-01-02 22:59:47 -0500

    complete

---
+++ app.js
@@ -0,0 +1,47 @@
+const input = document.querySelector('input');
+const adbtn = document.querySelector('.btn-add')
+const ul = document.querySelector('ul');
+const empty = document.querySelector('.empty');
+
+adbtn.addEventListener('click',(e) =>{
+    e.preventDefault();
+
+    const text = input.value;
+  
+    if (text !== "") {
+      const li = document.createElement("li");
+      const p = document.createElement("p");
+      p.textContent = text;
+  
+      li.appendChild(p);
+        //añadimos el boton eliminar a los elementos de la lista
+        li.appendChild(deletbtn());
+      ul.appendChild(li);
+     
+  
+      input.value = "";//vacia el input
+      empty.style.display = "none";//quita la palabra por defecto
+      
+     
+    }
+});
+
+function deletbtn(){
+    const eliminar = document.createElement("button");
+
+    eliminar.textContent = "X";
+    eliminar.className = "btn-delete";
+
+    eliminar.addEventListener("click", (e) => {
+        const item = e.target.parentElement;
+        ul.removeChild(item);
+    
+        const items = document.querySelectorAll("li");
+    
+        if (items.length === 0) {
+          empty.style.display = "block";
+        }
+      });
+    
+      return eliminar;
+}
