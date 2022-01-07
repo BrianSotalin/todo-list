2460764e7a7b154c5062c781ad805576f31f65bb
Brian Sotalin <dannysotalin@gmail.com>
2022-01-07 15:51:15 -0500

    dark-mode

--- app.js
+++ app.js
@@ -45,3 +45,10 @@ function deletbtn(){
     
       return eliminar;
 }
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+	document.body.classList.toggle('dark');//toggle agrega una clase si no tiene y la elimina si ya la tiene
+	btnSwitch.classList.toggle('active');
+});
+
